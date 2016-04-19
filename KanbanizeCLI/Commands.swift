5ccf3a9ffe4555942dfa6570ff645f335c09ddf8
Alexander Belyavskiy <diejmon@gmail.com>
2016-04-19 19:33:12 +0300

    Add cartfile and CLI library submodule

---
+++ KanbanizeCLI/Commands.swift
@@ -0,0 +1,14 @@
+//
+//  Commands.swift
+//  KanbanizeCLI
+//
+//  Created by Alexander Belyavskiy on 4/19/16.
+//  Copyright © 2016 Alexander Belyavskiy. All rights reserved.
+//
+
+import Foundation
+
+internal enum Command: String {
+  case LogTime
+  case AddComment
+}
---
+++ KanbanizeCLI/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  KanbanizeCLI
+//
+//  Created by Alexander Belyavskiy on 4/19/16.
+//  Copyright © 2016 Alexander Belyavskiy. All rights reserved.
+//
+
+import Foundation
+
+print(Args.all)
+
+NSRunLoop.currentRunLoop().run()
